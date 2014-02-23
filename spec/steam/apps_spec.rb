c55151f80e83a5245b9a8bf74945bb88e77b5d31
Brian Haberer <bhaberer@gmail.com>
2014-02-23 03:00:14 -0800

    testing for apps

--- spec/steam/apps_spec.rb
+++ spec/steam/apps_spec.rb
@@ -24,6 +24,11 @@ describe Steam::Apps do
       Steam::Apps.get_servers(addr: '192.168.1.1')
         .should == []
     end
+
+    it 'should capture json errors' do
+      Steam::Apps.get_servers(addr: nil)
+        .should == { error: '500 Internal Server Error' }
+    end
   end
 
   describe '.up_to_date' do
@@ -32,12 +37,24 @@ describe Steam::Apps do
         .should_not be_nil
     end
 
-    it 'should return a false up to date value if info' do
-      Steam::Apps.up_to_date(appid: 440, version: 10)
-        .should == { 'up_to_date' => false,
-                     'version_is_listable' => false,
-                     'required_version' => 2123087,
-                     'message' =>'Your server is out of date, please upgrade' }
+    it 'should return a false up to date value if the verion is out of date' do
+      check = Steam::Apps.up_to_date(appid: 440, version: 10)
+      check['up_to_date'].should be_false
+      check['version_is_listable'].should be_false
+      check.should have_key('required_version')
+      check['message'].should == 'Your server is out of date, please upgrade'
+    end
+
+    it 'should return a positive up to date value if the version is correct' do
+      current = Steam::Apps.up_to_date(appid: 440, version: 10)['required_version']
+      check = Steam::Apps.up_to_date(appid: 440, version: current)
+      check['up_to_date'].should be_true
+      check['version_is_listable'].should be_true
+    end
+
+    it 'should capture json errors' do
+      Steam::Apps.up_to_date(appid: nil, version: 'foo')
+        .should == { error: '500 Internal Server Error' }
     end
   end
 end
