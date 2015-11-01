ff139f15c31e3fd15f9494408d9e6a91a03aa9b3
Brian Haberer <bhaberer@gmail.com>
2015-11-01 02:01:50 -0800

    Why not use https

--- lib/steam-api/helpers.rb
+++ lib/steam-api/helpers.rb
@@ -4,7 +4,7 @@ module Steam
     # @param [String] api The endpoint of the API
     # @param [String] base_url the root uri for steam's API
     # @return [Steam::Client] The Client
-    def build_client(api, base_url: 'http://api.steampowered.com')
+    def build_client(api, base_url: 'https://api.steampowered.com')
       Steam::Client.new([base_url, api].join('/'))
     end
   end
