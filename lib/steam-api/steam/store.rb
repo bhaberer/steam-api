b3eccacd2b47c18de030cc1a4bfbd6a4552d5b51
Cory Kitchens <corykitchens@gmail.com>
2020-04-29 07:33:31 -0700

    Fixed rubocop errors and warnings

--- lib/steam-api/steam/store.rb
+++ lib/steam-api/steam/store.rb
@@ -3,17 +3,18 @@ module Steam
   # @see https://developer.valvesoftware.com/wiki/Steam_Web_API
   # @since 1.0.0
   module Store
-    #Get App Details
+    # Get App Details
     # @param String appid The UUID of the Steam Application
     # @see https://wiki.teamfortress.com/wiki/User:RJackson/StorefrontAPI#appdetails
     def self.app_details(appid)
-      response = client.get 'appdetails', params: { appids: appid}
-      response 
+      response = client.get 'appdetails', params: { appids: appid }
+      response
     end
 
     private
-      def self.client
-        build_client '', base_url: Steam::Helpers::STORE_API_BASE_URL
-      end
+
+    def self.client
+      build_client '', base_url: Steam::Helpers::STORE_API_BASE_URL
+    end
   end
 end
