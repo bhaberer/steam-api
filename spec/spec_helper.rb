e1bb3df908a640d20646cf9b1da3091d4e786dcf
Alexander Popov <alex.wayfer@gmail.com>
2022-10-28 15:08:54 +0300

    Enable RSpec `--only-fail` feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,7 @@ if File::exist? File.join(File.dirname(__FILE__), "secret.rb")
 end
 
 require 'steam-api'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+end
